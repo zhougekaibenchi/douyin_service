1dc3124eb7b6e220e783894d3db8dc75f06d74ca
mengyuan <2210549527@qq.com>
2022-11-09 18:04:27 +0800

    定时脚本bug修复

--- cron.py
+++ cron.py
@@ -16,7 +16,7 @@ ftp_trigger = CronTrigger(hour=12, minute=0)
 
 def main():
 
-    scheduler.add_job(func=douyin_pipeline, trigger=ftp_trigger, kwargs=[sys.argv[1]], #{'env':'test'}
+    scheduler.add_job(func=douyin_pipeline, trigger=ftp_trigger, kwargs={"env":sys.argv[1]}, #{'env':'test'}
                       name="数据同步", id='pipeline', max_instances=1, replace_existing=True)
     scheduler.add_listener(listener_event, EVENT_JOB_ERROR | EVENT_JOB_EXECUTED)
     try:
@@ -28,7 +28,7 @@ def main():
 
 def listener_event(event):
 
-    job = scheduler.get_job(event.job_id, trigger=ftp_trigger)
+    job = scheduler.get_job(event.job_id)
     if not event.exception:
         logger.info(f'{job.name}任务执行成功')
     else:
